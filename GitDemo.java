0a8ffcf19f95be1903722f2ec68961b21c189663
ThankfulCarp5 <72186925+ThankfulCarp5@users.noreply.github.com>
2021-02-26 16:55:31 -0700

    Revert "Swapped findFactorial and reverseNumber."

--- GitDemo.java
+++ GitDemo.java
@@ -25,10 +25,12 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 1;
+        double factorialNumber = 0;
         
-        for(int i = 1; i <= num; ++i){
-            factorialNumber = factorialNumber * i;
+        while( num != 0 ) {
+            double temp = num % 10;
+            factorialNumber = factorialNumber * 10 + temp;
+            num /= 10;
         }
         
         return factorialNumber;
@@ -40,16 +42,13 @@ public class GitDemo {
      *  the function will return 4321
      *****/
     public double reverseNumber(int num){
-        double newNumber = 0;
+        double newNumber = 1;
         
-        while( num != 0 ) {
-            double temp = num % 10;
-            newNumber = newNumber * 10 + temp;
-            num /= 10;
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
         }
         reverseNum = newNumber;
         return newNumber;
-        
     }
     
 }
